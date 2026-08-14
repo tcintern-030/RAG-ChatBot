e85efd184cd7ff7d81619f5f6dbee1f619b39a98
Ahmad Mustafa <ahmadmutafa15@gmail.com>
2026-08-14 18:31:28 +0500

    Final Updated Retrieval Pipeline of my ChatBot

--- retrival.py
+++ retrival.py
@@ -1,29 +1,54 @@
+import os
+
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 
+
+VECTOR_DB_DIR = "vector_db"
+COLLECTION_NAME = "rag_documents"
+
+
 def create_embedding_model():
+
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
     return embeddings
 
+
 def load_vector_db():
+
+    if not os.path.exists(VECTOR_DB_DIR):
+
+        raise FileNotFoundError(
+            "Vector database not found. "
+            "Please upload and process a document first."
+        )
+
+
     embeddings = create_embedding_model()
 
+
     vector_db = Chroma(
-        persist_directory="vector_db",
+        collection_name=COLLECTION_NAME,
+        persist_directory=VECTOR_DB_DIR,
         embedding_function=embeddings
     )
 
+
     return vector_db
 
+
 def retrieve_documents(query, k=7):
+
     vector_db = load_vector_db()
+
 
     documents = vector_db.similarity_search(
         query,
         k=k
     )
+
 
     return documents
